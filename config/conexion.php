2b64edda4a4fba0c784912a7d09f6b7e1acf9427
Arthur <enmanuelgauto@gmail.com>
2025-02-28 11:02:27 -0300

    terminando

--- config/conexion.php
+++ config/conexion.php
@@ -5,7 +5,8 @@
 
         protected function Conexion(){
             try{
-                $conectar = $this->dbh=new PDO("sqlsrv:Server=Arthur\SQLEXPRESS;Database=CompraVenta","sa","lacabragauto123");
+                /* $conectar = $this->dbh=new PDO("sqlsrv:Server=Arthur\SQLEXPRESS;Database=CompraVenta","sa","lacabragauto123"); */
+                $conectar = $this->dbh=new PDO("sqlsrv:server = tcp:gauriv01.database.windows.net,1433; Database = compraventa01","Gauriv","Yosoylacabra123#");
                 return $conectar;
             }catch(Exception $e){
                 print "Error Conexion BD". $e->getMessage()."<br/>";
@@ -14,7 +15,9 @@
         }
 
         public static function ruta(){
-            return "http://localhost:90/PERSONAL_CompraVenta/";
-        }
+            /* return "http://localhost:90/PERSONAL_CompraVenta/"; */
+            return "https://gaurivtech.azurewebsites.net/";
+            
+        }   
     }
 ?>
